b90cd317500dc22474f7be1e3c2e78eb2b697209
Luca Isidoro <apprendista44@gmail.com>
2020-06-10 17:34:18 +0200

    aggiunta commenti

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
 $(document).ready(function() {
+  //tendina compare andando sopra col mouse
   $('.with-dropdown').mouseenter(
     function () {
       $(this).children('.dropdown').addClass('active');
     }
   )
+  //scompare con un click
   $('.with-dropdown').click(
     function () {
       $(this).children('.dropdown').removeClass('active');
+    }
+  )
+  //cambia l'icona con la freccia quando ci vado sopra col mouse
+  $('.it').mouseenter(
+    function () {
+      $(this).children('i').toggleClass('active');
+    }
+  )
+  $('.it').mouseleave(
+    function () {
+      $(this).children('i').toggleClass('active');
     }
   )
 });
