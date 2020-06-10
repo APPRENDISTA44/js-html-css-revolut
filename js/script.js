ed31ae0e4611afdbcf1ca87f1c541c3750789d21
Luca Isidoro <apprendista44@gmail.com>
2020-06-10 16:53:46 +0200

    miglioramenti vari

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
   $('.with-dropdown').mouseenter(
     function () {
-      $(this).children('.dropdown').toggleClass('active');
+      $(this).children('.dropdown').addClass('active');
+    }
+  )
+  $('.with-dropdown').click(
+    function () {
+      $(this).children('.dropdown').removeClass('active');
     }
   )
 });
